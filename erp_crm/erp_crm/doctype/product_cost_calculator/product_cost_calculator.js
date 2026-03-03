acfdbe88258823f47f705210b12084b66019c00f
Ali Naqi <alinaqi@Alis-MacBook.local>
2026-03-03 23:05:54 +0500

    Create Product and Calculator doctypes

---
+++ erp_crm/erp_crm/doctype/product_cost_calculator/product_cost_calculator.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ali & Talal and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Product Cost Calculator", {
+// 	refresh(frm) {
+
+// 	},
+// });
